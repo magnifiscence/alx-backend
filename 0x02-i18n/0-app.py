0ea864d849558effa50e8367b1b478b1892569ff
tata <tdivine284@gmail.com>
2024-04-11 02:11:11 +0000

    internalisation with python

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Basic Flask app"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Index page"""
+    return render_template('0-index.html')
---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Basic Flask app"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config(object):
+    """Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Get locale"""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """Index page"""
+    return render_template('4-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
